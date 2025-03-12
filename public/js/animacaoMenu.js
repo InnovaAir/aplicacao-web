1931cab12e217f16e425729702443fc7a9427aa4
henriquedandrade <henrique.barros@sptech.school>
2025-03-11 22:25:33 -0300

    Correção da função menu Hamburguer nas demais telas

---
+++ public/js/animacaoMenu.js
@@ -0,0 +1,8 @@
+function abrirMenu() {
+    document.getElementById("menu").classList.add("menu-aberto");
+    document.getElementById("tela").classList.add("tela-movida");
+  }
+  function fecharMenu() {
+    document.getElementById("menu").classList.remove("menu-aberto");
+    document.getElementById("tela").classList.remove("tela-movida");
+  }
