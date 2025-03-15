8bc4186bee57a1a1f038a574f8e8ff2993f9285f
vinicius201002 <vinicius.opedroso@sptech.school>
2025-03-15 17:17:29 -0300

    implementação do menu nas telas de dashboard

--- public/js/animacaoMenu.js
+++ public/js/animacaoMenu.js
@@ -6,3 +6,14 @@ function abrirMenu() {
     document.getElementById("menu").classList.remove("menu-aberto");
     document.getElementById("tela").classList.remove("tela-movida");
   }
+
+  function abrirMenuMobile() {
+    const menuMobile = document.querySelector('.menu_mobile');
+    const novoMenu = document.querySelector('.novo_menu');
+
+    if (novoMenu.style.display === 'flex') {
+        novoMenu.style.display = 'none';
+    } else {
+        novoMenu.style.display = 'flex';
+    }
+}
